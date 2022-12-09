f999fd06c2d4b3511af43c4bf2a657c2e7fcad9d
melonpan <73855155+melonpan@users.noreply.github.com>
2022-12-09 13:31:36 -0300

    funciona2

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
 from app.main import app
+import telegram_bot_integration
+import multiprocessing
 
-if __name__ == "__main__":
-        app.run()
+if __name__ == '__main__':
+     webapp_proc = multiprocessing.Process(name='webapp_proc', target=app.run)
+     telegram_proc = multiprocessing.Process(name='telegram_proc', target=telegram_bot_integration.main)
+     webapp_proc.start()
+     telegram_proc.start()
