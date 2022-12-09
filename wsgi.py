2eeb4a54d2f6d0e14f13284ea113f1d307249131
melonpan <73855155+melonpan@users.noreply.github.com>
2022-12-09 13:48:16 -0300

    multiproc em apenas 1 arquivo

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,12 @@
 from app.main import app
-import telegram_bot_integration
-import multiprocessing
+# import telegram_bot_integration
+# import multiprocessing
+
+# if __name__ == '__main__':
+#      webapp_proc = multiprocessing.Process(name='webapp_proc', target=app.run)
+#      telegram_proc = multiprocessing.Process(name='telegram_proc', target=telegram_bot_integration.main)
+#      telegram_proc.start()
+#      webapp_proc.start()
 
 if __name__ == '__main__':
-     webapp_proc = multiprocessing.Process(name='webapp_proc', target=app.run)
-     telegram_proc = multiprocessing.Process(name='telegram_proc', target=telegram_bot_integration.main)
-     telegram_proc.start()
-     webapp_proc.start()
+    app.run()
