8db40da8797db889cb5c4a9d3335aba95f98c30e
melonpan <73855155+melonpan@users.noreply.github.com>
2022-12-09 02:00:53 -0300

    cfg env

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/melon/projects/cactobot/env"
+set -gx VIRTUAL_ENV "/home/melon/projects/cactoshop_bot/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
